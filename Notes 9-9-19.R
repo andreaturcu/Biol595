cc2947ab5eb207efaaf853c4c68a4fa8220579b1
andreaturcu <aleathead@gmail.com>
2019-09-16 16:05:36 -0500

    Class Notes 9-16-19

--- Notes 9-9-19.R
+++ Notes 9-9-19.R
@@ -78,3 +78,37 @@ unique(fish$year)
 install.packages("data.table")
 ?mean
 getwd()
+#######NOTES 9-16-19
+d<-read.table(file= 'ISIIS201405291242.txt', sep="\t", skip=10, header=TRUE, fileEncoding="ISO-8859-1", stringsAsFactors = FALSE, quote = "\"", check.names = FALSE, encoding = "UTF-8",na.strings = "9999.99")
+#\t means that our file is tab delimited, which means our columns in the file are spaced out by tabs
+#na.strings tell R that if it sees this value then it'll turn it into NA
+head(d)
+#Make sure to write true and false in capital letters
+date<- scan(file='ISIIS201405291242.txt', what="character", skip = 1, nlines=1, quiet=TRUE)
+date
+#The goal of the above was to pull out the date from line 2 in our file
+date<-date[2]
+date
+#next, we need to put the date into a format that R recognizes as a date, so we need to break it apart and put it back together in a way that R recognizes
+library("stringr")
+#above allows you to walk along a string and pull out different parts
+mm<- str_sub(string= date, start =1, end=2)
+d$constant=NULL
+mm
+dd<- str_sub(date, 4,5)
+dd
+yy<-str_sub(date, 7, 8)
+yy
+dateNextDay <-str_c(mm,as.character(dd+1),yy,sep="/")
+dd<-as.numeric(dd)
+dateNextDay
+#as.numeric changes the dd to a number, MUST do this first
+head(d)
+str(d)
+d$hour<- as.numeric(str_sub(d$Time,1,2))
+str(d$Time)
+names(d)
+head(d$hour)
+head(d$Time)
+d$min<-as.numeric(str_sub(d$Time,4,5))
+d$sec<-as.numeric(str_sub(d$Time,7,11))
