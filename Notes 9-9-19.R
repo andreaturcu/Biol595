218c43ae4196992ec7d8c5e680e89553d30d6f1e
andreaturcu <aleathead@gmail.com>
2019-10-09 21:13:18 -0500

    Notes 10-9-19

--- Notes 9-9-19.R
+++ Notes 9-9-19.R
@@ -224,4 +224,207 @@ m2$seq <- seq(from =1, to =nrow(m2), by=1)
 v<-seq(5, 20, 0.5)
 vc <- cut(x=v, breaks =seq(5,20,0.5), include.lowest = T)
  #####Today we've gone over subsetting, used merge and join, we talked about a cut function to bin your data
- 
+ #####################Notes 10-2
+ install.packages("nutshell")
+library(nutshell)
+data("batting.2008")
+d<- batting.2008
+#tapply --- (tidyverse function)        
+#find quantile vales for home runs by team
+hr <- tapply(X = d$HR, INDEX = list(d$teamID), FUN = sum)
+hr 
+#fivenum (see below) gives you: min, lower-hinge, median, upper-hinge, and max value
+hr.q <- tapply(X = d$HR, INDEX = list(d$teamID), FUN = fivenum)
+hr.q  
+#In the one below we're looking for the hits compared to the at bats for the league ID (Kelly wrote: one category summarize)
+lg.q <- tapply(X = (d$H/d$AB), INDEX = list(d$lgID), FUN = fivenum)        
+lg.q    
+#We could do the same thing using summary and subsetting- subsetting within subsetting within subsetting
+summary(d[d$lgID== "AL",]$H/d[d$lgID== "AL",]$AB)
+#(Kelly wrote: two category summaries)
+bats <- tapply(X =d$HR, INDEX= list(d$lgID, d$bats), FUN= mean)
+bats        
+#ABOVE: THE POINT IS THAT WE WERE ABLE TO SUMMARIZE OVER TWO CATEGORIES (you could do this with two categories!)
+#(Kelly wrote: three category summaries)
+bats.team <- tapply(X =d$HR, INDEX= list(d$lgID, d$bats, d$team), FUN= mean)
+bats.team        
+#ABOVE: Added a third category in there  
+#Only use tapply if you're trying to use one category over a set of vecors
+#aggregate ----        
+
+team.stats <- aggregate(x = d[c("AB","H","BB","2B","HR")],by=list(d$teamID), FUN=sum)
+team.stats
+#aggregate and summarise are similar; see below for example
+
+#summarise is a tidyverse function
+# tidyverse summarise() ----
+team.sum = d %>% group_by(teamID) %>% summarise(ABsum =sum(AB), ABmean= mean(AB),ABsd = sd(AB),ABcount =n())
+#ABOVE:  %>% allows you to take a dataframe and send it down the pipeline (pass the information from the dataframe d to group_by)
+
+#rowsum ----
+#when you just want to add up the values in each row
+rs = rowsum(d[,c("AB","H","HR","2B","3B")], group =d$teamID)
+rs
+
+#counting variables ----
+#use the function "tabluate"
+ HR.cnts <- tabulate(d$HR)
+ names(HR.cnts) <-0:(length(HR.cnts)-1)
+
+#table ----
+table(d$bats)
+table(d[,c("bats","throws")])
+
+#aside about the names function ----
+m <- matrix(nrow = 4, ncol = 3)
+colnames(m) <- c("one","two","three")
+rownames(m) <- c("apple","pear","orange","berry")
+m
+
+#reshaping your data ----
+n <- matrix(1:10, nrow = 5)
+n
+#you can transpose your data just like in excel where your columns become your rows:
+t(n)
+#if you do the above with a vector then you'll turn it into a list with one row and a string of values going down
+
+#Another way to reshape your data is by stacking:
+s <- d[,c("lgID","teamID","AB","HR","throws")]
+s.un <- unstack(x =s, form = HR ~ AB)
+###NOT SURE WHAT WE WERE TRYING TO DO ABOVE, IT WENT WRONG IN CLASS
+
+library(reshape2)
+
+#use the "dcast" function to change data frame from the long to wide format
+s.wide <- dcast(data =s, value.var = "HR" formula = "lgID" ~ "teamID", fun.aggregate = mean)
+####ABOVE DID NOT WORK IN CLASS BUT WE RAN OUT OF TIME TO TROUBLESHOOT
+####OVERALL: TODAY WE WENT OVER SUMMARISE AND DID A LITTLE RESHAPING WITH THE TRANSPOSING
+####HOMEWORK: WILL BE DUE ON MONDAY!
+
+              
+##Notes 10-7-19
+f <- fish; rm(fish)
+fs <- f[,c("transect.id","area_fac","depth_fac","parcel.id","parcel.density.m3","parcel.length.m")]
+fs
+#How to rename a field:
+library(tidyverse)
+fs<-rename(.data=fs,tid=transect.id)
+names(fs)
+fs<-rename(.data=fs, area = area_fac)
+fs<-rename(.data=fs, depth=depth_fac)
+fs<-rename(.data=fs, pid=parcel.id)
+fs<-rename(.data=fs, pl=parcel.length.m)
+fs<-rename(.data=fs, pd =parcel.density.m3)
+
+
+###Reshaping your data ----
+library(reshape2)
+#using the function 'melt' (reshape2) to change 
+#used for heading that are categorical and then contain numbers, Kelly wrote: data frame from wide to long format
+
+fs.melt <- melt(data=fs,id.vars =c("tid","pid","area","depth"), measure.vars=c("pl","pd"),value.name=c("numbers"))
+fs.melt
+
+#if you want to go the other direction, your data is in the long format and you want to turn it into the wide
+#use dcast function
+fs.cast <- dcast(data=fs.melt,formula=tid~variable,value.var =c("numbers"), fun.aggregate=mean)
+fs.cast
+head(fs.cast)
+names(fs.melt)
+
+#spread and gather using tidyverse:
+fs.gather=fs %>% group_by(tid,area,depth,pid)%>% gather(key = 'varaible',value = 'value',pd,pl)
+head(fs.gather)
+
+#removing duplicates ----
+head(fs)
+o1<-fs[1,]
+o2<-fs[1,]
+o3<-fs[1,]
+o4<-fs[2:10,]
+
+#bind these individual objects back together using function "rbind"
+o<-rbind(o1,o2,o3,o4)
+#now the first three rows are duplicate observations
+no.dups<- o[!duplicated(o),]
+dups <- o[duplicated(o),]              
+
+
+#complete cases
+#this is used to look for lines where there is no missing data, no NA's allowed
+#Below: making some na data
+fs[2,]$pd <-NA
+fs[4,]$pl <-NA
+fs.complete <- fs[complete.cases(fs),]              
+head(fs.complete)              
+#note that in the output in the pid column, number 2 and 4 are gone because it got rid of the na's
+
+#sorting data ----
+attach(mtcars)
+mtcars              
+
+#sort by mpg
+nd<-mtcars[order(mpg),]
+nd              
+
+#using the arrange funtion
+nd.arrange <- arrange(.data=mtcars,mpg) #ascending
+nd.arrange 
+nd.arrange.desc <-arrange(.data=mtcars, desc(mpg)) #descending
+nd.arrange.desc              
+
+#sorting by two things:
+nd.m.c <- arrange(.data=mtcars, mpg, cyl)
+nd.m.c              
+#today we went over shaping, melt and dcast, we renamed column, removed duplicates, complete cases (throwing out observations with missing data), and sorted using order and arrange              
+  
+  
+########Notes 10-9-19#######
+#Exam Review
+
+#The dates and times we went over before exam 1:
+d= read.table(ect, ect, ect)(see original notes for whole thing)
+#sep=\t means tab delimited, and if you left a blank space there then you'd have a space delimited file.
+#header=TRUE you put when you have column headers
+#strings as factors=FALSE means: a factor is a variable with different levels, so LOOK UP! NEEDS MORE INFO
+#na.strings=9999.99 means that any cell that has this value will be counted as an NA
+
+#How to creat a proper date and time function:
+#date = scan(isis314325134.txt, what = 'chatacter', skip =1, nlines = 1, quiet =TRUE)
+#mm =str_sub(string = date, start =1, end = 2)
+#with a string and you're telling it that you want to start at 1 and end at 5 then all positions count, even spaces!
+#You can switch to a character instead of numeric (see dateNextday from original notes)
+
+#if you want to pull the last two characters of a string you can put -1 and -2 instead of counting them from the front
+#above: make sure you have start = -2 and end = -1 so they're in the correct orrder (last # is -1, 5th to last if -5, ect)
+
+#d$Time is set as a string of numbers with hour, min, sec format
+#so if you want to pull out the hour information you'd put: d$hour = as.numeric(str_sub(d$Time,1,2))
+
+#combining two strings, the date from one column, and the time from another column:
+#do that using the: as.POSIxct(strptime(d$dateTime, format= "%m/%d/%y %H:%M:%OS, tz="America/New_York"))
+
+#to change the time zone: d$changetime= format(d$dateTime, tz = "America/Los_Angeles
+
+##Aggregate:
+#c = aggregate(x= df$H, by = list(df$teamID), FUN = sum)
+#The output of aggregate is a dataframe
+
+a2.df = data.frame(tid = row.names(a), mean = as.numeric(a))
+#above creates a new data frame
+
+#####to look something up in R studio you can type: ?summarise or whatever you're looking for
+
+
+#merging data frames
+q=data.frame(id=c('a','b','c','d'), num=c(1,2,3,4))
+v=data.frame(id=c('a','b','e'), car =c(6,7,8))
+qv= merge(x=q, y= v, by = 'id')
+qv
+#it only combined a and b because those were the only ones that were in common
+#you could include all = T into the above qv= equation to get all of the data whether it has a match or not, if it doesn't have a match then it's an NA
+# or you could include all.x=T and it'll put everything from the 'left' side and tries to match it to y's or will put NA
+
+#melt and cast ----
+library(reshape2)
+m= melt(data =df, idvars=c('teamID'), measure.vars =c('H', 'AB'))
